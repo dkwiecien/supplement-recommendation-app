8310bf99c729eab6ff219284781dd25163960168
Dominik Kwiecień <242448@edu.p.lodz.pl>
2024-04-24 23:50:10 +0200

    feat: add header and background toggle

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,tsx,vue}'],
+  content: ['./index.html', './src/**/*.{ts,tsx,vue}'],
   theme: {
     extend: {}
   },
